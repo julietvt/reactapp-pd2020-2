50aa2be7247d47c40e117edaa8cd3124898dccf8
julietvt <freshitpt@gmail.com>
2021-03-07 11:30:34 +0200

    composition

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import Greeting from './components/Greeting';
 
 function App() {
-  return <Greeting name={'Vasya'} surname={'Ivanov'} />;
+  return (
+    <>
+      <Greeting name={'Vasya'} surname={'Ivanov'} />
+      <Greeting name={'Masha'} surname={'Ivanova'} />
+      <Greeting name={'Barsik'} surname={'Ivanovvvvvv'} />
+    </>
+  );
 }
 
 export default App;
--- src/components/Greeting/index.jsx
+++ src/components/Greeting/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import styles from './style.module.sass'
+
+/*
+function Greeting(props) {
+  const {name, surname} = props;
+  return <div>Hello, {name} {surname} !!!</div>;
+}
+*/
 
 const Greeting = props => {
     const {name, surname} = props;
     return <div>Hello, {name} {surname} !!!</div>;
 };
 
+/*
+class Greeting extends React.Component {
+  render() {
+    return <div>Hello, {this.props.name} {this.props.surname} !!!</div>;
+  }
+}
+*/
 export default Greeting;
