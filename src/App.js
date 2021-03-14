7560d78a69565f40ef0c3b282dc90046d4a78ede
julietvt <freshitpt@gmail.com>
2021-03-14 13:50:28 +0200

    подьем состояния

--- src/App.js
+++ src/App.js
@@ -2,13 +2,54 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import UserList from './components/UserList';
+import SelectedUsers from './components/SelectedUsers';
 
-function App() {
-  return (
-    <>
-      <UserList />
-    </>
-  );
+const dbUsers = [
+  {
+    id: 1,
+    fName: 'firstName1',
+    lName: 'lastName1',
+  },
+  {
+    id: 2,
+    fName: 'firstName2',
+    lName: 'lastName2',
+  },
+  {
+    id: 3,
+    fName: 'firstName3',
+    lName: 'lastName3',
+  },
+  {
+    id: 4,
+    fName: 'firstName4',
+    lName: 'lastName4',
+  },
+  {
+    id: 5,
+    fName: 'firstName5',
+    lName: 'lastName5',
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: dbUsers.map((u) => ({ ...u, isSelected: false })),
+    };
+  }
+  setUsers = (newUsers) => {
+    this.setState({ users: newUsers });
+  };
+  render() {
+    const { users } = this.state;
+    return (
+      <>
+        <UserList users={users} setUsers={this.setUsers} />
+        <SelectedUsers users={users} />
+      </>
+    );
+  }
 }
-
 export default App;
--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -1,56 +1,18 @@
 import React, { Component } from 'react';
 import UserItem from '../../components/UserItem';
-const dbUsers = [
-                {
-                    id: 1,
-                    fName: 'firstName1',
-                    lName: 'lastName1',
-                },
-                {
-                    id: 2,
-                    fName: 'firstName2',
-                    lName: 'lastName2',
-                },
-                {
-                    id: 3,
-                    fName: 'firstName3',
-                    lName: 'lastName3',
-                },
-                {
-                    id: 4,
-                    fName: 'firstName4',
-                    lName: 'lastName4',
-                },
-                {
-                    id: 5,
-                    fName: 'firstName5',
-                    lName: 'lastName5',
-                },
-            ];
-class UserList extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            users: dbUsers.map(u=>({...u, isSelected:false})),
-        };
-    }
-    mapUser = (user, index) => {
-        const {users} = this.state;
-        const selectUsersHandler = () => {
-            const newUsers = [...users];
-            this.setState({users: newUsers});
-        };
-        return(
+const UserList = (props) => {
+    const {users, setUsers} = props;
+    const mapUser = (user, index) => {
+    const selectUsersHandler = () => {
+        const newUsers = [...users];
+        newUsers[index].isSelected = !newUsers[index].isSelected;
+        setUsers(newUsers);
+    };
+    return(
             <UserItem key={user.id} user={user} onSelectUsers={selectUsersHandler} isSelected={user.isSelected} />
-        );
-    }
-    
-  render() {
-      const {users} = this.state;
-    return (
-      <ul> {users.map(this.mapUser)} </ul>
     );
-  }
-}
+    };
+    return <ul> {users.map(mapUser)} </ul>
+  };
 
 export default UserList;
