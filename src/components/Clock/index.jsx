72df0658e3a95c195eb8e7ac8787a082922a6918
julietvt <freshitpt@gmail.com>
2021-03-21 13:45:49 +0200

    refactor: clock component with hooks useState and useEffect

--- src/components/Clock/index.jsx
+++ src/components/Clock/index.jsx
@@ -1,62 +1,28 @@
-import React, {Component} from 'react';
+import { startOfYear } from 'date-fns';
+import { addMilliseconds } from 'date-fns/esm';
+import React, {useState, useEffect} from 'react';
 import styles from './Clock.module.scss';
-class Clock extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            isRunning: false,
-            time: new Date(0,0,0,0,0,0,0,0),
-        };
-        this.timeoutId = null;
-    }
-    tick= () => {
-        const {time} = this.state;
-        this.setState({
-            time: new Date(time.getTime()+1000),
-        })
-    };
-    start = () => {
-        if(!this.state.isRunning){
-            this.setState({
-                isRunning: true,
-            });
-        }
-    };
-    stop = () => {
-        if(this.state.isRunning){
-            this.setState({
-                isRunning: false,
-            });
-        }
-    };
-    clear = () => {
-        if(this.timeoutId){
-            clearTimeout(this.timeoutId);
-            this.timeoutId = null;
-        }
-    };
-    reset = () => {};
-    componentDidMount(){ this.start(); }
-    componentDidUpdate(){
-        const {isRunning} = this.state;
-        this.clear();
+function Clock(){
+    const [time, setTime] = useState(new Date(0,0,0,0,0,0,0,0));
+    const [isRunning, setIsRunning] = useState(false);
+    useEffect( () => {
         if(isRunning){
-            this.timeoutId = setTimeout(this.tick, 500);
+            const timeoutId = setInterval(() => {
+                setTime((prevValue) => addMilliseconds(prevValue,1000));
+            },1000);
+            return ()=>{
+                clearInterval(timeoutId);
+            };
         }
-    }
-    componentWillUnmount(){ this.clear(); }
-    render(){
-        const {time, isRunning} = this.state;
-        return(
-            <article className={styles.wrapper}>
-                <div className={styles.time}>
-                    {time.toLocaleTimeString('it-IT')}
-                </div>
-                <button disabled={isRunning} onClick={this.start}>start</button>
-                <button disabled={!isRunning} onClick={this.stop}>stop</button>
-                <button onClick={this.reset}>reset</button>
-            </article>
-        )
-    }
+    }, [isRunning, time]);    
+    return(
+        <div>
+            <h1>{format(time, 'HH:mm:ss')}</h1>
+            <button onClick={() => setIsRunning(!isRunning)}>
+                {isRunning ? 'stop' : 'start'}
+            </button>
+        </div>
+    );
 }
+
 export default Clock;
