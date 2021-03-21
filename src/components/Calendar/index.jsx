bfe0622652b5456a9449cb7e50ca825540784190
julietvt <freshitpt@gmail.com>
2021-03-21 12:19:37 +0200

    add hint

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,6 +1,25 @@
 import React, {Component} from 'react';
 import {format, add, getWeeksInMonth, getDaysInMonth} from 'date-fns';
 
+/*
+getWeeks({date, currentDate}){
+eachWeekOfInterval(
+start: startOfMonth(date)
+end: endOfMonth(date)
+).map
+
+<Week key={weekDate}
+weekDate={weekDate}
+date={date}
+currentDate={currentDate}
+/>
+}
+
+const Month = props => {
+const {date}=props;
+}
+*/
+
 const Calendar = ({currentDate}) => {
     return(
         <article>
